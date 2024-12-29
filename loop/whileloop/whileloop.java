77382f53b488c10f3626f6e9546044939e817adf
Mohamed Thahir <thahirsprojects@gmail.com>
2024-12-28 17:07:13 -0800

    whileloop

---
+++ loop/whileloop/whileloop.java
@@ -0,0 +1,17 @@
+package loop.whileloop;
+
+/*While loop are typicaly used when the number of iterations is not known before the loop starts 
+*/
+
+public class whileloop {
+    public static void main(String[] args) {
+        
+        int i = 0;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+
+        }
+    }
+    
+}
