353688c681b41f7647e8df41a453812aa8ac8937
Mohamed Thahir <thahirsprojects@gmail.com>
2024-12-29 07:52:33 -0800

    -

---
+++ functions/functionwithparameter/cal.java
@@ -0,0 +1,34 @@
+package functionwithparameter;
+
+public class cal {
+
+    void sum(int a,int b){
+            System.out.println(a+"+"+b+"="+(a+b));
+        }
+
+    void sub(int a, int b) {
+        System.out.println(a+"-"+b+"="+(a-b));
+    }
+
+    void mul(int a,int b){
+        System.out.println(a+"*"+b+"="+(a*b));
+    }
+
+    void div(int a,int b){
+        System.out.println(a+"/"+b+"="+(a/b));
+    }
+
+    public static void main(String[] args) {
+
+        cal obj = new cal();
+
+        obj.sum(23, 34);
+
+        obj.sub(45, 54);
+
+        obj.mul(23, 23);
+
+        obj.div(100, 10);
+
+    }
+}
