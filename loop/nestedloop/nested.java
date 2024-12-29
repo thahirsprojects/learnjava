e1542cd92adf002dd03751feb607af5ca51cb6fe
Mohamed Thahir <thahirsprojects@gmail.com>
2024-12-28 16:36:55 -0800

    nestedloop

---
+++ loop/nestedloop/nested.java
@@ -0,0 +1,29 @@
+/*Loop inside a loop is nested loop */ 
+
+package loop.nestedloop;
+
+import java.util.Scanner;
+
+/*printing 
+ *  *
+ *  **
+ *  ***
+ *  ****
+*/
+
+public class nested {
+    public static void main(String[] args) {
+
+        Scanner scan = new  Scanner(System.in);
+        System.out.print("Enter a number :");
+        int num = scan.nextInt();
+        for(int i=1;i<=num;i++){
+
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
