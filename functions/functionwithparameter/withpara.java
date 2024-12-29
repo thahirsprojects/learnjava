7045acbcd2027a285ceff3dab23f06cca953beaf
Mohamed Thahir <thahirsprojects@gmail.com>
2024-12-29 07:43:32 -0800

    parameter

---
+++ functions/functionwithparameter/withpara.java
@@ -0,0 +1,21 @@
+package functionwithparameter;
+
+public class withpara {
+        void getsoap(int money){
+            System.out.println(money);
+            System.out.println("Soap Purchashed");
+        }
+
+        void getchocolate(int money){
+            System.out.println(money);
+            System.out.println("Chocolate Purchashed");
+        }
+        public static void main(String[] args) {
+             
+            withpara obj = new withpara();
+            obj.getsoap(46);
+            obj.getchocolate(20);
+
+
+    }
+}
