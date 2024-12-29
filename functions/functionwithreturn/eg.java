0f687bb5f0b42529cfc9d6ca555f866097c08e6a
Mohamed Thahir <thahirsprojects@gmail.com>
2024-12-29 08:24:44 -0800

    -

---
+++ functions/functionwithreturn/eg.java
@@ -0,0 +1,18 @@
+package functionwithreturn;
+
+public class eg {
+
+    String getname(){
+        return "Thahir";
+    }
+
+    public static void main(String[] args) {
+        
+        eg obj = new eg();
+
+        String name = obj.getname();
+
+        System.out.println(name);
+    }
+    
+}
